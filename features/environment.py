bb44ad3ea14636a848f16b447cfc776265f04c25
zhammer <zachary_hammer@alumni.brown.edu>
2018-12-22 11:46:46 -0500

    Add basic behave environment

---
+++ features/environment.py
@@ -0,0 +1,27 @@
+import os
+
+import behave
+
+from features.support.fixtures import (
+    with_aws_lambda_environment_variables,
+    with_empty_db
+)
+
+
+TEST_DATABASE_CONNECTION_STRING = os.environ.get(
+    'TEST_DATABASE_CONNECTION_STRING',
+    'sqlite:///morning_cd_behave_tests.db'
+)
+
+
+def before_scenario(context: behave.runner.Context, scenario: behave.model.Scenario) -> None:
+    behave.use_fixture(
+        with_aws_lambda_environment_variables,
+        context,
+        TEST_DATABASE_CONNECTION_STRING
+    )
+    behave.use_fixture(
+        with_empty_db,
+        context,
+        TEST_DATABASE_CONNECTION_STRING
+    )
